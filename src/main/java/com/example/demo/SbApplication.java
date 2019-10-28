3ca641abbf8ae033a6b201c47a279e04e1fcce84
as.yang <yanganshihf@163.com>
2019-10-28 17:49:06 +0800

    引入jpa支持

--- src/main/java/com/example/demo/SbApplication.java
+++ src/main/java/com/example/demo/SbApplication.java
@@ -9,5 +9,5 @@ public class SbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +22,15 @@ public class TestController {
     public Test dealTest(@RequestParam(value="name", defaultValue="World") String name) {
         return new Test(counter.incrementAndGet(),
                             String.format(template, name));
-    }@RequestMapping("/test1")
+    }
+    @RequestMapping("/test1")
     public Test dealTest1(@RequestParam(value="name", defaultValue="World") String name) {
         return new Test(counter.incrementAndGet(),
                             String.format(template, name));
     }
-    
+    @RequestMapping("/aaa")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "aaa";
+    }
 }
