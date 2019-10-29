36e8e9418cb7febd420e4d53316d2120077c8f55
as.yang <yanganshihf@163.com>
2019-10-29 09:41:50 +0800

    实现一个闭环请求

---
+++ src/main/java/com/ruitesen/demo/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.ruitesen.demo.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ruitesen.demo.mapper.CategoryMapper;
+import com.ruitesen.demo.model.Category;
+
+@Controller
+public class CategoryController {
+	@Autowired CategoryMapper categoryMapper;
+	
+	@RequestMapping("/listCategory")
+    public String listCategory(Model m) throws Exception {
+        List<Category> cs=categoryMapper.findAll();
+          
+        m.addAttribute("cs", cs);
+          
+        return "listCategory";
+    }
+}
