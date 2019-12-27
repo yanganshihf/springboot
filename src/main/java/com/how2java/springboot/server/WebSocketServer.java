311385a641723305586a9c974939bbac7d8ebae4
as.yang <yanganshihf@163.com>
2019-12-27 13:24:32 +0800

    更新server类的方法

--- src/main/java/com/how2java/springboot/server/WebSocketServer.java
+++ src/main/java/com/how2java/springboot/server/WebSocketServer.java
@@ -1,16 +1,20 @@
 package com.how2java.springboot.server;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 @ServerEndpoint("/websocket/{userId}")
 @Component
 public class WebSocketServer {
@@ -44,6 +48,70 @@ public class WebSocketServer {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+     * 收到客户端消息后触发的方法
+     * @param message 客户端发送过来的消息
+     */
+    @OnMessage
+    public void onMessage(String message) {
+        System.out.println("来自客户端的消息:" + message);
+        //群发消息
+        try {
+            if (StringUtils.isEmpty(message)){
+                return ;
+            }
+            //如果给所有人发消息携带@ALL, 给特定人发消息携带@xxx@xxx#message
+            String[] split = message.split("#");
+            if (split.length>1){
+                String[] users = split[0].split("@");
+                if (users.length<2){return;}
+                String firstuser = users[1].trim();
+                if (StringUtils.isEmpty(firstuser)||"ALL".equals(firstuser.toUpperCase())){
+                    String msg =username +": "+ split[1];
+                    sendInfo(msg);//群发消息
+                }else{//给特定人员发消息
+                    for (String user : users) {
+                        if (!StringUtils.isEmpty(user.trim())){
+                            sendMessageToSomeBody(user.trim(),split[1]);
+                        }
+                    }
+                }
+            }else{
+                sendInfo(username +": "+message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+    	System.out.println("发生错误 session: "+session);
+        error.printStackTrace();
+    }
+    
+    // 给特定人员发送消息
+    public void sendMessageToSomeBody(String username,String message) throws IOException {
+        if(users.get(username)==null){
+            return;
+        }
+        users.get(username).session.getBasicRemote().sendText(message);
+        this.session.getBasicRemote().sendText(this.username+"@"+username+": "+message);
+    }
+
+    /**
+     * 群发自定义消息
+     */
+    public  void sendInfo(String message) throws IOException {
+        for (WebSocketServer item : users.values()) {
+            try {
+                item.session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                continue;
+            }
+        }
+    }
 	
 	/**
 	 * 连接关闭调用方法
